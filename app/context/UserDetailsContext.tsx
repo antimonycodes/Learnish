267b37cabf6c0a46a940e32598d8fed601fc14d2
antimonycodes <akinrinadetobilobasb@gmail.com>
2025-08-06 08:19:41 +0100

    `Update UserDetailContext type and default value`

--- app/context/UserDetailsContext.tsx
+++ app/context/UserDetailsContext.tsx
@@ -1,3 +1,9 @@
+// app/context/UserDetailsContext.tsx
 import { createContext } from "react";
 
-export const UserDetailContext = createContext(null);
+type UserDetailContextType = {
+  userDetail: any;
+  setUserDetail: React.Dispatch<React.SetStateAction<any>>;
+} | null;
+
+export const UserDetailContext = createContext<UserDetailContextType>(null);
